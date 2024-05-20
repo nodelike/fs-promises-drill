108e2a5e730dd686e0873768b96db4f59ccc8662
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-20 14:09:42 +0530

    Refactored the fs-promise problem1.js to use async await and then create and delete random files.

--- problem1.js
+++ problem1.js
@@ -1,27 +1,27 @@
 const fs = require('fs').promises
 
-function createOutputDirectory(){
-    return fs.mkdir("output", { recursive: true})
+async function createOutputDirectory(){
+    await fs.mkdir("output", { recursive: true})
 }
 
-function createRandomFiles(){
+async function createRandomFiles(){
     let createFilePromises = [];
     for(let i = 0; i < 10; i++){
         let data = {id: i, filename: `random-file-${i}.json`};
         createFilePromises.push(fs.writeFile(`output/random-file-${i}.json`, JSON.stringify(data, null, 2)))
     }
 
-    return Promise.all(createFilePromises);
+    await Promise.all(createFilePromises);
 }
 
-function deleteFiles(){
+async function deleteFiles(){
     let deleteFilePromises = [];
 
     for(let i = 0; i < 10; i++){
         deleteFilePromises.push(fs.unlink(`output/random-file-${i}.json`))
     }
 
-    return Promise.all(deleteFilePromises);
+    await Promise.all(deleteFilePromises);
 }
 
 
